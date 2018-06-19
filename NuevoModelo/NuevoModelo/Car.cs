f97d4ee938123970397f7c60258c8720b3739ca1
DESKTOP-AFUL3NK\jaiba <JavierIbarra>
2018-06-19 18:43:12 -0400

    Proyecto DLL

---
+++ NuevoModelo/NuevoModelo/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuevoModelo
+{
+    class Car
+    {
+        private string marca;
+        private string modelo;
+        private int año;
+        private Persona dueño;
+
+        public Car(string Nmarca, string Nmodelo, int Naño, Persona Ndueño)
+        {
+            this.marca = Nmarca;
+            this.modelo = Nmodelo;
+            this.año = Naño;
+            this.dueño = Ndueño;
+        }
+
+        public string Marca
+        {
+            get { return marca; }
+        }
+
+        public string Modelo
+        {
+            get { return modelo; }
+        }
+
+        public int Año
+        {
+            get { return año; }
+        }
+
+        public Persona Dueño
+        {
+            get { return dueño; }
+            set { dueño = value; }
+        }
+    }
+}
---
+++ NuevoModelo/NuevoModelo/Persona.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuevoModelo
+{
+    public class Persona
+    {
+        private string nombreCompleto;
+        private string nombre;
+        private string apellido;
+        private string rut;
+        private int edad;
+
+        public Persona(string Nnombre, string Napellido, string Nrut, int Nedad)
+        {
+            this.nombre = Nnombre;
+            this.apellido = Napellido;
+            this.rut = Nrut;
+            this.edad = Nedad;
+        }
+
+        public void NombreCompleto()
+        {
+            string nombrecompleto = nombre + " " + apellido;
+            this.nombreCompleto = nombrecompleto;
+        }
+
+        public string Resultado
+        {
+            get { return nombreCompleto; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        public string Rut
+        {
+            get { return rut; }
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+        }
+    }
+}
